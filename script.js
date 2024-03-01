bc3d0a3ad1b5f8567bf0bda9e6e937bec52a51d6
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-03-01 20:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,58 +1,39 @@
 //your JS code here. If required.
-const circles = document.querySelectorAll('.circle');
-const bars = document.querySelectorAll('.bar');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-
-let currentCircleIndex = 0;
-
-
-function updateProgress() {
-    circles.forEach((circle, index) => {
-        if (index === currentCircleIndex) {
-            circle.classList.add('active');
-        } else {
-            circle.classList.remove('active');
-        }
-    });
-
-    bars.forEach((bar, index) => {
-        if (index < currentCircleIndex) {
-            bar.classList.add('active');
-        } else {
-            bar.classList.remove('active');
-        }
-    });
-
-    
-    if (currentCircleIndex === 0) {
-        prevButton.disabled = true;
-    } else {
-        prevButton.disabled = false;
-    }
-
-    if (currentCircleIndex === circles.length - 1) {
-        nextButton.disabled = true;
-    } else {
-        nextButton.disabled = false;
-    }
+let prev=document.getElementById('prev');
+let next=document.getElementById('next');
+let circles=document.querySelectorAll('.circle');
+let bars=document.querySelectorAll('.bar');
+let currentIndex=0;
+function updateProgress(){
+	circles.forEach((circle,index)=>{
+		if(index<=currentIndex){
+			circle.classList.add('active');
+		}
+		else{	
+			circle.classList.remove('active');
+		}
+	});
+	bars.forEach((bar,index)=>{
+		if(index<currentIndex){
+			bar.classList.add('active');
+		}
+		else{	
+			bar.classList.remove('active');
+		}
+	});
 }
 
-
-nextButton.addEventListener('click', () => {
-    if (currentCircleIndex < circles.length - 1) {
-        currentCircleIndex++;
-        updateProgress();
-    }
-});
-
-
-prevButton.addEventListener('click', () => {
-    if (currentCircleIndex > 0) {
-        currentCircleIndex--;
-        updateProgress();
-    }
-});
-
-
-updateProgress();
+next.addEventListener('click',()=>{
+	
+	if(currentIndex< circles.length-1){
+		currentIndex++;
+		updateProgress();
+	}
+})
+prev.addEventListener('click',()=>{;
+	if(currentIndex > 0){
+		currentIndex--;
+		updateProgress();
+	}
+	
+})
